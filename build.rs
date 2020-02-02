71e191ee73c7a5cea8e2e8091b119901be55e6de
fedotoff <fedotoff@ispras.ru>
2020-02-02 17:48:37 +0300

    Initial commit with bindings for x86/x86_64

---
+++ build.rs
@@ -0,0 +1,83 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    let host  = env::var("HOST").unwrap();
+    let split:Vec<&str> = target.split('-').collect();
+    let arch = split[0];
+    let sys = split[2];
+    let abi = split[3];
+    //check system libunwind library for linux only
+    if sys != "linux" {
+        println!("cargo:warning=libunwind supports only Linux");
+        return;
+    }
+    
+    //set include directory
+    println!("cargo:rustc-link-search=libunwind/src/.libs");
+
+    //choose build
+    let (link_lib_arch,link_lib_abi) = match (arch, abi) {
+        ("x86_64","gnu") => ("x86_64",""),
+        ("x86_64","musl") => ("x86_64","=static"),
+        ("i586","gnu")|("i686","gnu") => ("x86",""),
+        ("i586","musl")|("i686","musl") => ("x86","=static"),
+        ("arm", "gnueabi")|("armv7", "gnueabi") => ("arm",""),
+        ("arm", "musleabi")|("armv7", "musleabi") => ("arm","=static"),
+        _ => ("","")
+    };
+    if link_lib_arch.is_empty() {
+        println!("cargo:warning=target {} is unsupported",target);
+        return;
+    }
+    //build C libunwind
+    Command::new("./autogen.sh").current_dir(&Path::new("./libunwind").canonicalize().unwrap()).status().unwrap();
+    //configure. Check if we compile for  x86 target on x86_64 host
+    if link_lib_arch == "x86" && host.contains("x86_64") {
+        Command::new("./configure").current_dir(&Path::new("./libunwind").canonicalize().unwrap())
+            .arg("CFLAGS=-m32")
+            .arg(&format!("--target={}",target))
+            .arg(&format!("--host={}",target)).status().unwrap();
+    //configure. Check if we compile for  arm target on x86_64 host
+    } else  if link_lib_arch == "arm" && host.contains("x86_64") {
+        Command::new("./configure").current_dir(&Path::new("./libunwind").canonicalize().unwrap())
+            .arg("CC=arm-linux-gnueabi-gcc")
+            .arg(&format!("--target={}",target))
+            .arg(&format!("--host={}",target))
+            .arg("--disable-tests").status().unwrap();
+    }
+    else {
+        Command::new("./configure").current_dir(&Path::new("./libunwind").canonicalize().unwrap()).arg(&format!("--target={}",target)).status().unwrap();
+    }
+
+    let status = Command::new("make").current_dir(&Path::new("./libunwind").canonicalize().unwrap()).status().expect("failed to execute make");
+    if !status.success() {
+        println!("cargo:warning=build is failed");
+        return;
+    }
+    println!("cargo:rustc-link-lib{}=unwind-coredump",link_lib_abi);
+    println!("cargo:rustc-link-lib{}=unwind-{}",link_lib_abi,link_lib_arch);
+
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("libunwind/include/libunwind-coredump.h")
+        //include directory
+        .clang_arg("-Ilibunwind/include")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,133 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[cfg(test)]
+mod tests {
+    extern crate libc;
+
+    use super::*;
+    use libc::c_char;
+    use std::ffi::CStr;
+    use std::ffi::CString;
+    use std::mem::MaybeUninit; 
+    use std::path::PathBuf;
+
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn test_core_unwind() {
+        unsafe {
+            let mut libc_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            libc_path_buf.push("data/libc-2.23.so");
+            let mut test_callstack_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            test_callstack_path_buf.push("data/test_callstack");
+            let mut core_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            core_path_buf.push("data/core.test_callstack");
+
+            let test_callstack_start:u64 = 0x400000;
+            let libc_start:u64 = 0x00007f9ac7468000;
+            let test_callstack_path = CString::new(test_callstack_path_buf.to_str().unwrap()).unwrap();
+            let libc_path = CString::new(libc_path_buf.to_str().unwrap()).unwrap();
+            let core_path = CString::new(core_path_buf.to_str().unwrap()).unwrap();
+
+            println!("{:#?}\n{:#?}\n{:#?}",test_callstack_path,libc_path, core_path);
+            let asp = _Ux86_64_create_addr_space(&mut _UCD_accessors ,0);
+            let ui: * mut UCD_info = _UCD_create(core_path.as_ptr());
+            let mut c  = MaybeUninit::uninit();
+            let mut ret = _Ux86_64_init_remote(c.as_mut_ptr(),asp,ui as * mut libc::c_void );
+            _UCD_add_backing_file_at_vaddr(ui, test_callstack_start, test_callstack_path.as_ptr());
+            
+            _UCD_add_backing_file_at_vaddr(ui, libc_start, libc_path.as_ptr());
+           let mut ip: unw_word_t = 0;
+           loop {
+           _Ux86_64_get_reg(c.as_mut_ptr(), unw_frame_regnum_t_UNW_REG_IP as ::std::os::raw::c_int, &mut ip);
+              let mut off  = MaybeUninit::uninit();
+              let mut name_vec:Vec<c_char> = vec![0;64];
+              _Ux86_64_get_proc_name(c.as_mut_ptr(), name_vec.as_mut_ptr(),64, off.as_mut_ptr());
+              let name = CStr::from_ptr(name_vec.as_mut_ptr());
+              println!("0x{:x} in {:?} ()", ip, name.to_str().unwrap());
+              ret = _Ux86_64_step(c.as_mut_ptr());
+              if ret <= 0 {
+                  break;
+              }
+           }
+        }
+    }
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn test_core_unwind_heap_error() {
+        unsafe {
+            let mut libc_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            libc_path_buf.push("data/libc-2.23.so");
+            let mut test_heap_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            test_heap_path_buf.push("data/test_heapError");
+            let mut core_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            core_path_buf.push("data/core.test_heapError");
+
+            let test_heap_start:u64 = 0x000055b7b218c000;
+            let libc_start:u64 = 0x00007f90e058b000;
+            let test_heap_path = CString::new(test_heap_path_buf.to_str().unwrap()).unwrap();
+            let libc_path = CString::new(libc_path_buf.to_str().unwrap()).unwrap();
+            let core_path = CString::new(core_path_buf.to_str().unwrap()).unwrap();
+            let asp = _Ux86_64_create_addr_space(&mut _UCD_accessors ,0);
+            let ui: * mut UCD_info = _UCD_create(core_path.as_ptr());
+            let mut c  = MaybeUninit::uninit();
+            let mut ret = _Ux86_64_init_remote(c.as_mut_ptr(),asp,ui as * mut libc::c_void );
+            _UCD_add_backing_file_at_vaddr(ui, test_heap_start, test_heap_path.as_ptr());
+            
+            _UCD_add_backing_file_at_vaddr(ui, libc_start, libc_path.as_ptr());
+           let mut ip: unw_word_t = 0;
+           loop {
+           _Ux86_64_get_reg(c.as_mut_ptr(), unw_frame_regnum_t_UNW_REG_IP as ::std::os::raw::c_int, &mut ip);
+              let mut off  = MaybeUninit::uninit();
+              let mut name_vec:Vec<c_char> = vec![0;64];
+              _Ux86_64_get_proc_name(c.as_mut_ptr(), name_vec.as_mut_ptr(),64, off.as_mut_ptr());
+              let name = CStr::from_ptr(name_vec.as_mut_ptr());
+              println!("0x{:x} in {:?} ()", ip, name.to_str().unwrap());
+              ret = _Ux86_64_step(c.as_mut_ptr());
+              if ret <= 0 {
+                  break;
+              }
+           }
+        }
+    }
+    #[test]
+    #[cfg(target_arch = "x86_64")]
+    fn test_core_unwind_canary() {
+        unsafe {
+            let mut libc_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            libc_path_buf.push("data/libc-2.23.so");
+            let mut test_canary_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            test_canary_path_buf.push("data/test_canary");
+            let mut core_path_buf  = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+            core_path_buf.push("data/core.test_canary");
+
+            let test_canary_start:u64 = 0x0000558672376000;
+            let libc_start:u64 = 0x00007fc14b336000;
+            let test_canary_path = CString::new(test_canary_path_buf.to_str().unwrap()).unwrap();
+            let libc_path = CString::new(libc_path_buf.to_str().unwrap()).unwrap();
+            let core_path = CString::new(core_path_buf.to_str().unwrap()).unwrap();
+            let asp = _Ux86_64_create_addr_space(&mut _UCD_accessors ,0);
+            let ui: * mut UCD_info = _UCD_create(core_path.as_ptr());
+            let mut c  = MaybeUninit::uninit();
+            let mut ret = _Ux86_64_init_remote(c.as_mut_ptr(),asp,ui as * mut libc::c_void );
+            _UCD_add_backing_file_at_vaddr(ui, test_canary_start, test_canary_path.as_ptr());
+            
+            _UCD_add_backing_file_at_vaddr(ui, libc_start, libc_path.as_ptr());
+           let mut ip: unw_word_t = 0;
+           loop {
+           _Ux86_64_get_reg(c.as_mut_ptr(), unw_frame_regnum_t_UNW_REG_IP as ::std::os::raw::c_int, &mut ip);
+              let mut off  = MaybeUninit::uninit();
+              let mut name_vec:Vec<c_char> = vec![0;64];
+              _Ux86_64_get_proc_name(c.as_mut_ptr(), name_vec.as_mut_ptr(),64, off.as_mut_ptr());
+              let name = CStr::from_ptr(name_vec.as_mut_ptr());
+              println!("0x{:x} in {:?} ()", ip, name.to_str().unwrap());
+              ret = _Ux86_64_step(c.as_mut_ptr());
+              if ret <= 0 {
+                  break;
+              }
+           }
+        }
+    }
+}
